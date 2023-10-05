4be3ff9d7b1012f1fc19901ae5dd095816259c6a
Yogesh <45172882+yogeshxd@users.noreply.github.com>
2023-10-06 00:17:16 +0530

    Add files via upload

---
+++ api/api.py
@@ -0,0 +1,24 @@
+from flask import Flask, request,jsonify
+import pickle
+import re
+
+app = Flask("__name__")
+app.debug = True
+
+model = pickle.load(open('phishing.pkl', 'rb'))
+
+def predictweb(url):
+    url = []
+    url.append(url)
+    result = model.predict(url)
+    return result[0]
+
+@app.route('/predict/<url>', methods=['GET', 'POST'])
+def predict(url):
+    url = re.sub(r'https?://(?:www\.)?', '', url).split('/')[0]
+    prediction = model.predict([url])[0]
+    return jsonify(prediction)
+
+
+if __name__ == '__main__':
+    app.run()
